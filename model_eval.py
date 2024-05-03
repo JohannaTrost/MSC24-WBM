ff4ffc7d366579bebe9a0b9e7282debf85ff4067
jophi24 <johanna.philipps@web.de>
2024-05-03 11:37:32 +0200

    LAI update

--- model_eval.py
+++ model_eval.py
@@ -3,6 +3,7 @@ import numpy as np
 import netCDF4 as nc
 from run import time_evolution
 
+
 cs_values = [210, 420, 840]
 alpha_values = [2, 4, 8]
 gamma_values = [0.2, 0.5, 0.8]
@@ -10,10 +11,11 @@ beta_values = [0.4, 0.6, 0.8]
 cm_values = [1.5, 2, 2.5]
 et_weights = [(.5, .5), (.25, .75), (.75, .25)]
 # define w_0
-years = np.arange(2000,2024,1)
+years = np.arange(2000,2018,1)
 P_data = []
 R_data = []
 T_data = []
+lai_data = []
 calibration_time = [2000,2010]
 
 # get radiation and precipitation data from netCDF files
@@ -34,19 +36,29 @@ for year in years:
     nc_file = nc.Dataset(file_path)
     T_data.append(nc_file.variables['t2m'][:,0,0])
     nc_file.close()
+    
+    file_path = 'data/lai/lai.daily.0d50_CentralEurope.'+str(year)+'.nc'
+    nc_file = nc.Dataset(file_path)
+    lai_data.append(nc_file.variables['lai'][:,0,0])
+    nc_file.close()
 
 
 # define dummy LAI with sinus function
-n_time_steps = np.concatenate(T_data).shape[0]
-freq = 2 * np.pi / 365  # Frequency of the curve for one year
-sinus_curve = .5 * np.sin(freq * np.arange(n_time_steps) + 5)
-sinus_curve += .8  # Centered at 0.8
-LAI = sinus_curve.copy()
+# n_time_steps = np.concatenate(T_data).shape[0]
+# freq = 2 * np.pi / 365  # Frequency of the curve for one year
+# sinus_curve = .5 * np.sin(freq * np.arange(n_time_steps) + 5)
+# sinus_curve += .8  # Centered at 0.8
+# LAI = sinus_curve.copy()
 
 # Flatten data
 R_data = np.concatenate(R_data)
 P_data = np.concatenate(P_data)
 T_data = np.concatenate(T_data)
+lai_data = np.concatenate(lai_data)
 
-output = time_evolution(0.9*420, P_data, R_data, 0, T_data, LAI, 420, 2,
+output = time_evolution(0.9*420, P_data, R_data, 0, T_data, lai_data, 420, 2,
         0.4, 0.2, 1.5, (.5, .5))
+
+output['evapotranspiration'].plot()
+
+
