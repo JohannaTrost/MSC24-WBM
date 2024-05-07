1833679d330bc52edfb4d09387b8bd2a3ad724bc
johanna.trost <johanna.trost@univ-lyon1.fr>
2024-05-07 12:34:02 +0200

    Precipitation data scaling (to double/half standard deviation)

--- prepro.py
+++ prepro.py
@@ -3,8 +3,35 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import netCDF4 as nc
-from datetime import datetime 
+from datetime import datetime
 from tqdm import tqdm
+
+
+def split_dataset(data, save=False, path=''):
+
+    # Convert to date object first
+    data['date'] = pd.to_datetime(data['date'])
+
+    # Split time series
+    df_2000_to_2011 = data[(data['date'].dt.year >= 2000) & (
+            data['date'].dt.year <= 2011)]
+    df_2012_to_2023 = data[(data['date'].dt.year >= 2012) & (
+            data['date'].dt.year <= 2023)]
+
+    if save:
+        # Print the first few rows of each DataFrame to verify
+        print("DataFrame from 2000 to 2011:")
+        print(df_2000_to_2011.head())
+
+        print("\nDataFrame from 2012 to 2023:")
+        print(df_2012_to_2023.head())
+
+        df_2000_to_2011.to_csv(f'{path}/precip_2000_2011.csv',
+                               index=False)
+        df_2012_to_2023.to_csv(f'{path}/precip_2012_2023.csv',
+                               index=False)
+
+    return df_2000_to_2011, df_2012_to_2023
 
 
 def load_precip_ncs(save=False):
@@ -16,7 +43,8 @@ def load_precip_ncs(save=False):
 
     for year in tqdm(years):
 
-        file_path = 'data/total_precipitation/tp.daily.calc.era5.0d50_CentralEurope.' + str(year) + '.nc'
+        file_path = 'data/total_precipitation/tp.daily.calc.era5.0d50_CentralEurope.' + str(
+            year) + '.nc'
 
         nc_file = nc.Dataset(file_path)
         lon = nc_file.variables['lon'][:]
@@ -33,7 +61,7 @@ def load_precip_ncs(save=False):
                     data.append([timestamp, lat[j], lon[k], tp])
 
         nc_file.close()
-    
+
     # Create DataFrame
     precip_df = pd.DataFrame(data, columns=['date', 'lat', 'lon', 'tp'])
 
@@ -42,18 +70,9 @@ def load_precip_ncs(save=False):
     print(precip_df.head())
 
     # Split df for saving 
-    if save:
-        df_2000_to_2011 = precip_df[(precip_df['date'].dt.year >= 2000) & (precip_df['date'].dt.year <= 2011)]
-        df_2012_to_2023 = precip_df[(precip_df['date'].dt.year >= 2012) & (precip_df['date'].dt.year <= 2023)]
+    df_2000_to_2011, df_2012_to_2023 = split_dataset(precip_df,
+                                                     save=save,
+                                                     path='data/'
+                                                          'total_precipitation')
 
-        # Print the first few rows of each DataFrame to verify
-        print("DataFrame from 2000 to 2011:")
-        print(df_2000_to_2011.head())
-
-        print("\nDataFrame from 2012 to 2023:")
-        print(df_2012_to_2023.head())
-
-        df_2000_to_2011.to_csv('data/total_precipitation/precip_2000_2011.csv', index=False)
-        df_2012_to_2023.to_csv('data/total_precipitation/precip_2012_2023.csv', index=False)
-    
     return precip_df
---
+++ scale_sd.py
@@ -0,0 +1,105 @@
+import os
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+from prepro import split_dataset
+
+P_data1 = pd.read_csv('data/total_precipitation/precip_2000_2011.csv')
+P_data2 = pd.read_csv('data/total_precipitation/precip_2012_2023.csv')
+P_data = pd.concat([P_data1, P_data2])
+
+
+def adjust_precipitation(precip, scale_factor=2, cutoff=1e-05):
+    # Step 1: Scale the data to double its variability
+    scaled_precip = precip * scale_factor
+
+    # Step 2: Adjust for negativity
+    adjusted_precip = np.where(scaled_precip < 0, 0, scaled_precip)
+    adjusted_mean = adjusted_precip.mean()
+
+    # Step 3: Iterative Offset Adjustment
+    while np.abs(adjusted_mean - precip.mean()) > cutoff:
+        offset = precip.mean() - adjusted_mean
+        adjusted_precip += offset
+        adjusted_precip = np.where(adjusted_precip < 0, 0, adjusted_precip)
+        adjusted_mean = adjusted_precip.mean()
+
+    # Step 4: Final Adjustment
+    if (adjusted_precip < 0).any():
+        min_value = adjusted_precip.min()
+        adjusted_precip -= min_value
+
+    return adjusted_precip
+
+
+cutoff = 1e-05
+
+# Scale precipitation standard deviation
+P_data = P_data.set_index(['lat', 'lon'])
+P_data_double_sd = P_data.copy()
+P_data_half_sd = P_data.copy()
+
+# A lot faster with group by
+for scale_factor, new_data in zip([2, 0.5], [P_data_double_sd, P_data_half_sd]):
+    print('Scale factor', scale_factor)
+    for location in tqdm(np.unique(P_data.index)):
+        sample = P_data[P_data.index == location]
+        scaled_precip = adjust_precipitation(sample['tp'],
+                                             scale_factor=scale_factor,
+                                             cutoff=cutoff)
+
+        new_data.loc[P_data.index == location, 'tp'] = scaled_precip
+
+# Compare stats
+precip_stats = P_data.groupby(['lat', 'lon']).describe()
+precip_dbl_stats = P_data_double_sd.groupby(['lat', 'lon']).describe()
+precip_half_stats = P_data_half_sd.groupby(['lat', 'lon']).describe()
+
+# Calculate the ratio of standard deviations
+ratio_std_dbl = precip_dbl_stats[('tp', 'std')] / precip_stats[('tp', 'std')]
+ratio_std_half = precip_half_stats[('tp', 'std')] / precip_stats[('tp', 'std')]
+ratio_mean_dbl = precip_dbl_stats[('tp', 'mean')] / precip_stats[('tp', 'mean')]
+ratio_mean_half = precip_half_stats[('tp', 'mean')] / precip_stats[
+    ('tp', 'mean')]
+
+# Plot resulting mean and std and compare
+fig, ax = plt.subplots(2, 2, figsize=(10, 10))
+
+ax[0, 0].bar(range(len(ratio_std_dbl)), ratio_std_dbl)
+ax[0, 1].bar(range(len(ratio_std_half)), ratio_std_half)
+ax[1, 0].bar(range(len(ratio_mean_dbl)), ratio_mean_dbl)
+ax[1, 1].bar(range(len(ratio_mean_half)), ratio_mean_half)
+
+# Plot a horizontal line
+ax[0, 0].axhline(y=2, color='r', linestyle='--', label='target factor')
+ax[0, 1].axhline(y=0.5, color='r', linestyle='--')
+ax[1, 0].axhline(y=0, color='r', linestyle='--')
+ax[1, 1].axhline(y=0, color='r', linestyle='--')
+
+ax[0, 0].set_title('Standard deviation factor (doubled)')
+ax[0, 1].set_title('Standard deviation factor (half)')
+ax[1, 0].set_title('Mean ratio (doubled)')
+ax[1, 1].set_title('Mean ratio (half)')
+
+# Show plot
+ax[0, 0].legend()
+plt.show()
+
+plt.savefig(f'figs/scaling_{cutoff}_cutoff.pdf')
+
+# Save preprocessed data
+path = 'data/total_precipitation/preprocessed'
+os.makedirs(path, exist_ok=True)
+
+df_2000_to_2011_doubled, df_2012_to_2023_doubled = split_dataset(
+    P_data_double_sd.reset_index())
+df_2000_to_2011_half, df_2012_to_2023_half = split_dataset(
+    P_data_half_sd.reset_index())
+
+df_2000_to_2011_doubled.to_csv(f'{path}/precip_dbl_2000_2011.csv', index=False)
+df_2000_to_2011_doubled.to_csv(f'{path}/precip_dbl_2012_2023.csv', index=False)
+df_2000_to_2011_half.to_csv(f'{path}/precip_half_2000_2011.csv', index=False)
+df_2012_to_2023_half.to_csv(f'{path}/precip_half_2012_2023.csv', index=False)
